8ae4c911c7c64efaaa81252faf2d01e434ed3a78
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-05-23 09:26:44 +0200

    test the job

--- config_africa/develop/runDynQual_global_on_snellius.sh
+++ config_africa/develop/runDynQual_global_on_snellius.sh
@@ -20,10 +20,10 @@
 
 
 # please set where you stored DYNQUAL scripts - DON'T FORGET TO CHANGE THIS
-SCRIPT_FOLDER=/home/edwin/github/edwinkost/DYNQUAL/DynQualModel/
+SCRIPT_FOLDER=/home/edwinaha/github/edwinkost/DYNQUAL/DynQualModel/
 
 # the configuration file
-INI_FILE=/home/edwin/github/edwinkost/DYNQUAL/config_africa/develop/dynqual_05min_offline_develop_parallel.ini
+INI_FILE=/home/edwinaha/github/edwinkost/DYNQUAL/config_africa/develop/dynqual_05min_offline_develop_parallel.ini
 
 # we activate the correct conda environment on eejit and many other settings
 . /home/edwin/load_all_default.sh
@@ -33,19 +33,18 @@ INI_FILE=/home/edwin/github/edwinkost/DYNQUAL/config_africa/develop/dynqual_05mi
 cd ${SCRIPT_FOLDER}
 
 
-MAIN_OUTPUT_FOLDER="/scratch-shared/edwin/dynqual_test/"
+MAIN_OUTPUT_FOLDER="/scratch-shared/edwinaha/dynqual_test/gswp3-w5e5/historical-reference/"
 
 
 # do some runs with parallelization
-
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M06 -clone M06  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M03 -clone M03  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M07 -clone M07  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M08 -clone M08  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M01 -clone M01  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M09 -clone M09  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M05 -clone M05  &
-python deterministic_runner_offline_vbonato.py ../config_africa/develop/dynqual_05min_offline_develop_parallel.ini -mod ${MAIN_OUTPUT_FOLDER}/M02 -clone M02  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M06 -clone M06 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M03 -clone M03 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M07 -clone M07 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M08 -clone M08 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M01 -clone M01 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M09 -clone M09 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M05 -clone M05 -startTime 1980-01-01 -endTime 2014-12-31  &
+python deterministic_runner_offline_vbonato.py ${INI_FILE} -mod ${MAIN_OUTPUT_FOLDER}/M02 -clone M02 -startTime 1980-01-01 -endTime 2014-12-31  &
 
 
 wait
