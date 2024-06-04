0bda99beb7eba4f68ff5fa14a2a3a47d9dcbd566
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-06-04 16:31:58 +0200

    add an example of merging

--- tools/merge_netcdf/example_bash_for_merging.sh
+++ tools/merge_netcdf/example_bash_for_merging.sh
@@ -9,6 +9,6 @@ outputDir="/scratch-shared/edwinaha/dynqual_simulations/w5e5/existing/1980-1984/
 python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1980-02-28" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
 
 # monthly resolution
-python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outMonthAvgNC" "1980-01-01" "1980-12-31" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
+python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outMonthAvgNC" "1980-01-31" "1980-12-31" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
 
 wait
