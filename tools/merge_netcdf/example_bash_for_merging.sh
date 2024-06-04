d35dd04dc8f0e880f0b9eec758c82001792a074f
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-06-04 16:29:25 +0200

    add an example of merging

--- tools/merge_netcdf/example_bash_for_merging.sh
+++ tools/merge_netcdf/example_bash_for_merging.sh
@@ -5,21 +5,10 @@ inputDirRoot="/scratch-shared/vbonato/dynqual_simulations/w5e5/existing/1980-198
 
 outputDir="/scratch-shared/edwinaha/dynqual_simulations/w5e5/existing/1980-1984/global/"
 
-python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1981-02-28" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -37 -35 52 38
+# daily resolution
+python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1980-02-28" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
 
+# monthly resolution
+python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outMonthAvgNC" "1980-01-01" "1980-12-31" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
 
-#~ # file_type, options are: outDailyTotNC, outMonthTotNC, outMonthAvgNC, outMonthEndNC, outAnnuaTotNC, outAnnuaAvgNC, outAnnuaEndNC
-#~ file_type  = str(sys.argv[3])
-#~ startDate  = str(sys.argv[4]) 
-#~ endDate    = str(sys.argv[5])
-
-
-#~ # starting and end dates
-#~ startDate  = str(sys.argv[4]) 
-#~ endDate    = str(sys.argv[5])
-
-#~ # list of netcdf files that will be merged:
-#~ netcdfList = str(sys.argv[6])
-
-#~ inputDirRoot = sys.argv[1] 
-#~ outputDir    = 
+wait
