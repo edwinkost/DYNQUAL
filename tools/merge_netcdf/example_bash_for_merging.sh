64252cb808e2ab8e3f62a8a44b932891c49f6e95
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-06-04 16:19:02 +0200

    add an example of merging

--- tools/merge_netcdf/example_bash_for_merging.sh
+++ tools/merge_netcdf/example_bash_for_merging.sh
@@ -2,9 +2,8 @@
 inputDirRoot="/scratch-shared/edwinaha/dynqual_test/gswp3-w5e5/historical-reference/"
 outputDir=${inputDirRoot}/global/
 
-#~ python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1984-12-31" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08 defined -37 -35 52 38
 
-python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1981-02-28" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08 defined -37 -35 52 38
+python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1981-02-28" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -37 -35 52 38
 
 
 #~ # file_type, options are: outDailyTotNC, outMonthTotNC, outMonthAvgNC, outMonthEndNC, outAnnuaTotNC, outAnnuaAvgNC, outAnnuaEndNC
