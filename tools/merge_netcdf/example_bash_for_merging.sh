62a9f445c8e3257d9f4a9221b29e0b25300c8032
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-06-06 10:50:42 +0200

    testing job for merging

--- tools/merge_netcdf/example_bash_for_merging.sh
+++ tools/merge_netcdf/example_bash_for_merging.sh
@@ -1,14 +1,7 @@
 
+startYear="1980"
+endYear="1984"
 inputDirRoot="/scratch-shared/vbonato/dynqual_simulations/w5e5/existing/1980-1984/"
-
-#~ outputDir=${inputDirRoot}/global/
-
 outputDir="/scratch-shared/edwinaha/dynqual_simulations/w5e5/existing/1980-1984/global/"
 
-# daily resolution
-python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outDailyTotNC" "1980-01-01" "1980-02-28" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
-
-# monthly resolution
-python merge_netcdf_general.py ${inputDirRoot} ${outputDir} "outMonthAvgNC" "1980-01-31" "1980-12-31" discharge,waterTemp NETCDF4 True 2 M01,M02,M03,M05,M06,M07,M08,M09 defined 300 -17 -35 52 38 &
-
-wait
+sbatch --export inputDirRoot=${inputDirRoot},outputDir=${outputDir},startYear=${startYear},endYear=${endYear} slurm_job_for_merging.sh
